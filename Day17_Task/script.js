f5e660385cddb04ceca654cd72b73170b7c3d2a1
NiteshTembe <niteshtembe555@gmail.com>
2023-02-24 13:30:26 +0530

    day 17 task final

---
+++ Day17_Task/script.js
@@ -0,0 +1,51 @@
+const disp = document.createElement("p")
+disp.id="counter"
+document.body.appendChild(disp)
+let count = 11;
+        let counter = document.getElementById("counter")
+        setTimeout(()=>{
+            counter.innerText=--count
+            setTimeout(()=>{
+                counter.innerText=--count
+                setTimeout(()=>{
+                    counter.innerText=--count
+                    setTimeout(()=>{
+                        counter.innerText=--count
+                        setTimeout(()=>{
+                            counter.innerText=--count
+                            setTimeout(()=>{
+                                counter.innerText=--count
+                                setTimeout(()=>{
+                                    counter.innerText=--count
+                                    setTimeout(()=>{
+                                        counter.innerText=--count
+                                        setTimeout(()=>{
+                                            counter.innerText=--count
+                                            setTimeout(()=>{
+                                                counter.innerText=--count
+                                                setTimeout(()=>{
+                                                    counter.innerText="Happy Birthday !"
+                                                    counter.style.color="blue"
+                                                    clearTimeout()
+                                                },1000)
+                                                clearTimeout()
+                                            },1000)
+                                            clearTimeout()
+                                        },1000)
+                                        clearTimeout()
+                                    },1000)
+                                    clearTimeout()
+                                },1000)
+                                clearTimeout()
+                            },1000)
+                            clearTimeout()
+                        },1000)
+                        clearTimeout()
+                    },1000)
+                    clearTimeout()
+                },1000)
+                clearTimeout()
+            },1000)
+            clearTimeout()
+        },1000)
+        clearTimeout()
