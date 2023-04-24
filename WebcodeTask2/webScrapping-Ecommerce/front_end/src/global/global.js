ead5c2556837f01c94c7c6e7b7c324fd8b0157ef
NiteshTembe <niteshtembe555@gmail.com>
2023-04-25 03:42:12 +0530

    apilink

--- WebcodeTask2/webScrapping-Ecommerce/front_end/src/global/global.js
+++ WebcodeTask2/webScrapping-Ecommerce/front_end/src/global/global.js
@@ -1,5 +1,2 @@
-// export const imgUrlBase = "https://www.themoviedb.org/t/p/w600_and_h900_bestv2";
-// export const imgUrlBase = "https://www.themoviedb.org/t/p/w220_and_h330_face";
-
 
 export const scrapDataApi = "https://webcode-backend-m18t.onrender.com/products";
